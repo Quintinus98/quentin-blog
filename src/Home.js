321b3c5981b520c84ce89e1363db68e1ab730894
David Obodo <obododavidsop@gmail.com>
2022-08-23 10:38:23 +0100

    added useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 
@@ -10,15 +10,24 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'Lorem ipsum...', author: 'Fidelis', id: 3}
   ]);
 
+  const [name, setName] = useState('David');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
 
+  useEffect(() => {
+    console.log('UseEffect ran');
+    console.log(name);
+  }, [name]);
+
   return ( 
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
       {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'David')} title="David's Blogs" /> */}
+      <button onClick={() => setName("Peter")}>Change name</button>
+      <p>{ name }</p>
     </div>
   );
 }
